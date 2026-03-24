4fbb1dca5acba87829a30954850ce3861ac729a0
Ashish Patil <patil.ashishsubhash2025@vitstudent.ac.in>
2026-03-24 22:41:39 +0530

    Added debug print statement

--- src/main/java/com/paint/main/MainApp.java
+++ src/main/java/com/paint/main/MainApp.java
@@ -19,7 +19,7 @@ public class MainApp {
    
 
     public static void main(String[] args) {
-        
+        System.out.println("DevOps Project Running");
         Point p = new Point(10, 20);
 
         Shape circle = new Circle(5, p) {} ;
